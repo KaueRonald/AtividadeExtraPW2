abd19cd762ff58b2677ad8192167072c8fdfe17f
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-05-26 17:10:07 -0300

    feat: modificando sidebar de acordo com figma e adicionando componente avatar

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,21 +1,22 @@
 
 import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
+import { Avatar } from '../Avatar';
 
 export function SideBar() {
   return (
     <aside className={styles.sidebar}>
       <img
           className={styles.cover}
-          src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+          src="src\public\backgroundAvatar.png"
       />
       
       <div className={styles.profile}>
-        <img src="https://github.com/fabioabrantes.png"/>
+      <Avatar />
 
-        <strong>Leslie Alexander</strong>
+        <strong>Kauê Ronald</strong>
 
-        <span>UI Designer</span>
+        <span>Full Stack Developer</span>
       </div>
 
       <footer className={styles.footer}>
