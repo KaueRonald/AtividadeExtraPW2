85601db306a182319d9903aa34db4b0977efccd8
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-05-26 17:09:22 -0300

    feat: criando componente com avatar para ser adicionado em post e na sidebar

---
+++ src/components/Avatar/index.jsx
@@ -0,0 +1,9 @@
+import styles from './index.module.css';
+
+export function Avatar() {
+    return (
+            <div className={styles.profile}>
+                <img src="https://github.com/kaueronald.png" />
+            </div>
+    )
+}
